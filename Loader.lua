202e162c0d8fa1ffce9f4a4b136dd6ede601f1de
upoffice <jarvislolbro@gmail.com>
2026-07-07 06:17:45 -0500

    Change GameId to PlaceId in Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,8 +1,8 @@
-local id = game.GameId
+local id = game.PlaceId
 local url = "https://raw.githubusercontent.com/upoffice/titania/main/games/" .. id .. ".lua"
 local script = game:HttpGet(url)
 local fn = loadstring(script)
 if not fn then
-  error("[titania] no game " .. id)
+  error("no game " .. id)
 end
 fn()
